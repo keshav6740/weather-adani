7cf90daa54f2480b14b7409ae899df81f490eedb
Keshav <138072394+keshav6740@users.noreply.github.com>
2025-06-09 21:49:53 +0530

    Update build_files.sh

--- build_files.sh
+++ build_files.sh
@@ -3,11 +3,8 @@
 # Exit immediately if a command exits with a non-zero status.
 set -e
 
-# --- FIX IS HERE ---
-# Use the specific versioned pip that matches your vercel.json runtime
-echo "Installing dependencies..."
-
 echo "Collecting static files..."
-python3.9 manage.py collectstatic --noinput --clear
+# The python environment is already active here
+python manage.py collectstatic --noinput --clear
 
 echo "Build finished."
